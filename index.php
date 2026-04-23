6c879b969c90ceac057503fc8276413cb7c7e276
ALFHA <alfhaminecraft@gmail.com>
2026-04-23 09:51:26 +0700

    add form (name and job)

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
-<head>
-    <title>My Site</title>
-</head>
 <body>
-    <h1 style="color: blue;">Welcome</h1>
-    <p>This is my website</p>
+
+<h2>Simple Form</h2>
+
+<form method="POST">
+  Name: <input type="text" name="name"><br><br>
+  Job: <input type="text" name="job"><br><br>
+  <button type="submit">Submit</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "<h3>Result:</h3>";
+    echo "Name: " . $_POST["name"] . "<br>";
+    echo "Job: " . $_POST["job"];
+}
+?>
+
 </body>
 </html>
