f6b830c630ded73dc145d391bd1eaf15c8d54cdf
ALFHA <alfhaminecraft@gmail.com>
2026-04-23 10:03:39 +0700

    add html + php name card

---
+++ process.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST["name"];
+$job = $_POST["job"];
+?>
+
+<!DOCTYPE html>
+<html>
+<body style="background-color: lightyellow; text-align: center;">
+
+<h1>Name Card</h1>
+
+<p><b>Name:</b> <?php echo $name; ?></p>
+<p><b>Job:</b> <?php echo $job; ?></p>
+
+</body>
+</html>
